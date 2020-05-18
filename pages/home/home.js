64d2aeb0d54c31383814602e1f15bbf3901b1455
shawn <shawn@qq.com>
2020-05-18 13:43:35 +0800

    网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,5 @@
 // pages/home/home.js
+import request from "../../service/network";
 Page({
 
   /**
@@ -12,7 +13,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    request({
+      url:'http://httpbin.org/get'
+    }).then(res=>{console.log(res);
+    }).catch(err=>{console.log(err);
+    })
   },
 
   /**
